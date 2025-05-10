9c952d50f88b02294491c8548f6329ec4c2b97e8
francesliu2017 <francesliu2017@gmail.com>
2025-05-10 12:40:25 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    }
+}
